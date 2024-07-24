0de7caae85a0f17027e1a05790c571e35bd31a63
Diyorbekoffice <diyorbekboymirzayev0@gmail.com>
2024-07-24 14:40:52 +0500

    new

---
+++ index.js
@@ -0,0 +1,135 @@
+// // 1-masala
+// let arr = [1, 2, 3, 4, 5];
+// let doubled = arr.map(num => num * 2);
+// console.log(doubled); 
+
+// // 2-masala
+// let arr = [1, 2, 3, 4, 5];
+// let reversed = arr.slice().reverse();
+// console.log(reversed);
+
+// // 3-masala
+// let arr = [1, -2, 3, -4, 5];
+// let nonNegative = arr.filter(num => num >= 0);
+// console.log(nonNegative);
+
+// // 4-masala
+// let arr = [1, 2, 3, 4, 5];
+// let sum = arr.reduce((acc, num) => acc + num, 0);
+// console.log(sum); 
+
+// // 5-masala
+// let arr = [1, 2, 3, 4, 5];
+// arr.push(6);
+// console.log(arr); 
+
+// // 6-masala
+// let arr = [1, 2, 3, 4, 5];
+// arr.pop();
+// console.log(arr); 
+
+// // 7-masala
+// let arr = [1, 2, 3, 4, 5];
+// arr.shift();
+// console.log(arr); 
+
+// // 8-masala
+// let arr = [1, 2, 3, 4, 5];
+// arr.unshift(0);
+// console.log(arr);
+
+// // 9-masala
+// let arr = [1, 2, 3, 4, 5];
+// let index = arr.indexOf(3);
+// console.log(index);
+
+// // 10-masala
+// let arr = [1, 2, 3, 4, 5];
+// let copied = arr.slice();
+// console.log(copied);
+
+// // 11-masala
+// let arr = [1, 2, 3, 4, 5];
+// let max = Math.max(...arr);
+// console.log(max);
+
+// // 12-masala
+// let arr = [1, 2, 3, 4, 5];
+// let min = Math.min(...arr);
+// console.log(min);
+
+// // 13-masala
+// let arr = [1, 2, 3, 4, 5];
+// let evenCount = arr.filter(num => num % 2 === 0).length;
+// console.log(evenCount);
+
+// // 14-masala
+// let arr = [1, 2, 3, 4, 5];
+// let oddCount = arr.filter(num => num % 2 !== 0).length;
+// console.log(oddCount);
+
+// // 15-masala
+// let arr = [0, 1, 2, 0, 3, 0, 4, 5];
+// let newArr = arr.filter(num => num !== 0);
+// console.log(newArr);
+
+
+// // 16-masala
+// let arr = [1, 2, 3, 4, 5];
+// let doubledElements = arr.flatMap(num => [num, num]);
+// console.log(doubledElements);
+
+// // 17-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let mid = Math.ceil(arr.length / 2);
+// let firs = arr.slice(0, mid);
+// let second = arr.slice(mid);
+// console.log(firs); 
+// console.log(second); 
+
+// // 18-masala
+// let arr = [1, 2, 3, 4, 5];
+// let product = arr.reduce((acc, num) => acc * num, 1);
+// console.log(product);
+
+// // 19-masala
+// let arr = [1, 2, 3, 4, 5];
+// let odd = arr.filter((_, index) => index % 2 !== 0);
+// console.log(odd);
+
+// // 20-masala
+// let arr = [1, 2, 3, 4, 5];
+// let even = arr.filter((_, index) => index % 2 === 0);
+// console.log(even);
+
+// // 21-masala
+// let arr = [1, 'a', 2, 'b', 3, 'c'];
+// let strings = arr.filter(item => typeof item === 'string');
+// console.log(strings);
+
+// // 22-masala
+// let arr = [1, 'a', 2, 'b', 3, 'c'];
+// let numbers = arr.filter(item => typeof item === 'number');
+// console.log(numbers);
+
+// // 23-masala
+// let arr = [1, 2, 3, 4, 5, 6];
+// let leng = arr.length;
+// console.log(leng);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
